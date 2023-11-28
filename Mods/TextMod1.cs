a906a2e0d566d50830ac749ce0f9808dcfb8effe
1pomegranate1 <0326zxc9090@gmail.com>
2023-11-29 02:58:56 +0900

    BleedEffect

---
+++ Mods/TextMod1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMod1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("모드 테스트 1 스크립트");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Mods/TextMod2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMod2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("모드 테스트 2 스크립트");
+        Debug.Log("아주 잘되네여 조아여~");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
